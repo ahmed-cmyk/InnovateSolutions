8f2b192f8884cc129b7d6b1ae9d687a052a4eb99
Ubuntu <ubuntu@ip-172-31-14-52.eu-central-1.compute.internal>
2020-07-24 18:16:54 +0000

    Committed

--- DjangoUnlimited/settings.py
+++ DjangoUnlimited/settings.py
@@ -27,9 +27,10 @@ load_dotenv('.env')
 SECRET_KEY = 'p-y$&3u8l04=z-*r#2t&+ff2#&c2)+6$$tmpqf#vo8s3n57dxt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=1))
 
-ALLOWED_HOSTS = ['ec2-18-216-115-206.us-east-2.compute.amazonaws.com', '127.0.0.1']
+ALLOWED_HOSTS = ['ec2-18-157-77-169.eu-central-1.compute.amazonaws.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
