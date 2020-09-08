486adfd3abd488c41def17c4d358eaa9625cfc6e
ahmed-cmyk <ikramahmed398@gmail.com>
2020-09-08 14:19:40 +0400

    Added outlook email again with one minor change

--- DjangoUnlimited/settings.py
+++ DjangoUnlimited/settings.py
@@ -71,21 +71,21 @@ INSTALLED_APPS = [
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'innovateDjango123@gmail.com'
-EMAIL_HOST_PASSWORD = 'innovatePassword123'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-DEFAULT_FROM_EMAIL = 'innovateDjango123@gmail.com'
-# EMAIL_USE_TLS = True
-# EMAIL_HOST = 'smtp.office365.com'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'innovateDjango123@gmail.com'
+# EMAIL_HOST_PASSWORD = 'innovatePassword123'
 # EMAIL_PORT = 587
-# EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-# EMAIL_HOST_USER = 'careerportal@murdoch.edu.au'
-# DEFAULT_FROM_EMAIL = 'careerportal@murdoch.edu.au'
-# SERVER_EMAIL = 'careerportal@murdoch.edu.au'
+# EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = False
+# DEFAULT_FROM_EMAIL = 'innovateDjango123@gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'murdoch-edu-au.mail.protection.outlook.com'
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = 'careerportal@murdoch.edu.au'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_USE_SSL = False
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 SITE_ID = 1  # needed for 'django.contrib.sites' to work for admin site
