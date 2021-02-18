3c4b8cc73997727b75281493530957d8d419462c
ahmed-cmyk <ikramahmed398@gmail.com>
2021-02-18 14:33:42 +0400

    Changed home banner

--- staticfiles/js/EditProfile.js
+++ staticfiles/js/EditProfile.js
@@ -13,6 +13,10 @@ function testEmailValidity(email) {
     return !emailReg.test(email);
 }
 
+function verifyGradDate(gradDate, todayDate) {
+    return (gradDate < todayDate);
+}
+
 $(document).ready(function () {
       $('#id_first_name').change(function () {
         var firstName = $(this).val();
@@ -45,14 +49,30 @@ $(document).ready(function () {
         }
     });
 
-    $('#id_personal_email').change(function () {
-        if(($(this).val() === '') || (!testEmailValidity($(this).val()))) {
+    $('#id_personal_email').focusout(function () {
+        var email = $(this).val();
+        if(testEmailValidity(email)) {
             $(this).css('border', '1px solid red');
             $('#personalEmailError').html('Please enter a valid email address').css('color', 'red');
         }
         else {
             $(this).css('border', '');
             $('#personalEmailError').html('').css('color', '');
+        }
+    });
+
+    $('#id_expected_graduation_date').focusout(function () {
+        var expDate = $(this).val();
+        var today = new Date();
+        var convDate = new Date(expDate);
+        if(verifyGradDate(convDate, today)) {
+            $(this).css('border', '1px solid red');
+            $('#gradError').html('Expected graduation date cannot be in the past').css('color', 'red');
+        }
+        else {
+            console.log("Success");
+            $(this).css('border', '');
+            $('#gradError').html('').css('color', '');
         }
     });
 });
@@ -75,8 +95,11 @@ $("#editStudentForm").submit(function (e) {
     var firstname = $("#id_first_name").val();
     var lastname = $("#id_last_name").val();
     var email = $("#id_personal_email").val();
+    var expDate = $('#id_expected_graduation_date').val();
+    var today = new Date();
+    var convDate = new Date(expDate);
 
-    if(isNumber(firstname) || isNumber(lastname) || containsNumber(firstname) || containsNumber(lastname) || testEmailValidity(email)) {
+    if(isNumber(firstname) || isNumber(lastname) || containsNumber(firstname) || containsNumber(lastname) || testEmailValidity(email) || verifyGradDate(convDate, today)) {
         e.preventDefault();
         alert("There are errors in the form");
     }
